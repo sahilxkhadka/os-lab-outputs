69713864557f056898022b4cfd06323abe8b8e73
Sahil Khadka <sahilkhadka767@gmail.com>
2023-07-08 22:49:05 +0545

    lab 5 out[ut 1 added

--- src/components/LabOutputs.jsx
+++ src/components/LabOutputs.jsx
@@ -1,118 +1,181 @@
-import html2canvas from "html2canvas"
-import jsPDF from 'jspdf';
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 import { useRef, useState } from "react";
 import labOutputsDetails from "../details";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import rightArrow from '../images/right-arrow.svg'
+import rightArrow from "../images/right-arrow.svg";
 
 function LabOutputs({ loginSuccess }) {
-    const slider = useRef(null);
-    const [activeIndex, setActiveIndex] = useState(0)
-    const name = localStorage.getItem("name")
-    const symbol = localStorage.getItem("symbol")
-    const logout = () => {
-        loginSuccess(false)
-        localStorage.setItem("verified", "false")
-    }
+	const slider = useRef(null);
+	const [activeIndex, setActiveIndex] = useState(0);
+	const name = localStorage.getItem("name");
+	const symbol = localStorage.getItem("symbol");
+	const logout = () => {
+		loginSuccess(false);
+		localStorage.setItem("verified", "false");
+	};
 
-    var settings = {
-        dots: false,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        autoplay: false,
-        className: "slider",
-        draggable: true,
-    };
-    const scrollToPrev = () => {
-        slider?.current?.slickPrev();
-    };
-    const scrollToNext = () => {
-        slider?.current?.slickNext();
-    };
-    return (
-        <>
-            <div>
-                <button onClick={logout} className="btn logout-btn">
-                    <span className="icon">
-                        <svg viewBox="0 0 24 24" height="24" width="24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21 12L13 12" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M18 15L20.913 12.087V12.087C20.961 12.039 20.961 11.961 20.913 11.913V11.913L18 9" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M16 5V4.5V4.5C16 3.67157 15.3284 3 14.5 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H14.5C15.3284 21 16 20.3284 16 19.5V19.5V19" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-                    </span>
-                    <span className="text">Logout</span>
-                </button>
-            </div>
-            <div className="navs-container">
-                {
-                    labOutputsDetails.map((item, index) => {
-                        return (
-                            <button className={`btn download-btn nav-btn ${activeIndex === index ? "focused-btn" : ""}`}  key={index} onClick = {() => setActiveIndex(index)}>
-                                Lab {index + 1}
-                            </button>
-                        )
-                    })
-                }
-            </div>
-            <div className="outputs-container">
-                <Slider ref={slider} {...settings} >
-                    {
-                        labOutputsDetails[activeIndex].map((lab, index) => {
-                            const downloadImage = () => {
-                                const element = document.getElementById(lab.id);
-                                html2canvas(element, { dpi: 300 }).then((canvas) => { // Increase the DPI to 300
-                                    const imgData = canvas.toDataURL('image/png');
-                                    const pdf = new jsPDF();
-                                    pdf.addImage(imgData, 'PNG', 0, 15);
-                                    pdf.save(`${lab.id}.pdf`);
-                                });
-                            };
+	var settings = {
+		dots: false,
+		infinite: false,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		autoplay: false,
+		className: "slider",
+		draggable: true,
+	};
+	const scrollToPrev = () => {
+		slider?.current?.slickPrev();
+	};
+	const scrollToNext = () => {
+		slider?.current?.slickNext();
+	};
+	return (
+		<>
+			<div>
+				<button onClick={logout} className='btn logout-btn'>
+					<span className='icon'>
+						<svg
+							viewBox='0 0 24 24'
+							height='24'
+							width='24'
+							fill='none'
+							xmlns='http://www.w3.org/2000/svg'
+							stroke='#ffffff'
+						>
+							<g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+							<g
+								id='SVGRepo_tracerCarrier'
+								stroke-linecap='round'
+								stroke-linejoin='round'
+							></g>
+							<g id='SVGRepo_iconCarrier'>
+								{" "}
+								<path
+									d='M21 12L13 12'
+									stroke='#ffffff'
+									stroke-width='2'
+									stroke-linecap='round'
+									stroke-linejoin='round'
+								></path>{" "}
+								<path
+									d='M18 15L20.913 12.087V12.087C20.961 12.039 20.961 11.961 20.913 11.913V11.913L18 9'
+									stroke='#ffffff'
+									stroke-width='2'
+									stroke-linecap='round'
+									stroke-linejoin='round'
+								></path>{" "}
+								<path
+									d='M16 5V4.5V4.5C16 3.67157 15.3284 3 14.5 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H14.5C15.3284 21 16 20.3284 16 19.5V19.5V19'
+									stroke='#ffffff'
+									stroke-width='2'
+									stroke-linecap='round'
+									stroke-linejoin='round'
+								></path>{" "}
+							</g>
+						</svg>
+					</span>
+					<span className='text'>Logout</span>
+				</button>
+			</div>
+			<div className='navs-container'>
+				{labOutputsDetails.map((item, index) => {
+					return (
+						<button
+							className={`btn download-btn nav-btn ${
+								activeIndex === index ? "focused-btn" : ""
+							}`}
+							key={index}
+							onClick={() => setActiveIndex(index)}
+						>
+							Lab {index === 3 ? index + 2 : index + 1}
+						</button>
+					);
+				})}
+			</div>
+			<div className='outputs-container'>
+				<Slider ref={slider} {...settings}>
+					{labOutputsDetails[activeIndex].map((lab, index) => {
+						const downloadImage = () => {
+							const element = document.getElementById(lab.id);
+							html2canvas(element, { dpi: 300 }).then((canvas) => {
+								// Increase the DPI to 300
+								const imgData = canvas.toDataURL("image/png");
+								const pdf = new jsPDF();
+								pdf.addImage(imgData, "PNG", 0, 15);
+								pdf.save(`${lab.id}.pdf`);
+							});
+						};
 
-                            return (
-                                <>
-                                    <div key={lab.id} className={`slide-container`}>
-                                        <div className="output-container">
-                                            <h2>{lab.labName}</h2>
-                                            <p align="center" style={{ color: '#fff' }}>(You can also edit the path name manually)</p>
-                                            <div className="output-terminal" id={lab.id}>
-                                                <blockquote className="output-path" spellCheck="false" contentEditable={true} key={lab.labName}>
-                                                    <span className="user-name">{name.split(" ")[0]}@laptop</span>
-                                                    <span className="mingw">MINGW64</span>
-                                                    <span className="file-path">/f/{name}/{symbol}/lab-files-os</span>
-
-                                                </blockquote>
-                                                <div className="output-trigger">
-                                                    $ gcc lab-{lab.labCount}.c
-                                                </div>
-                                                <pre className="output-main">
-                                                    {lab.labOutput}
-                                                </pre>
-                                            </div>
-                                        </div>
-                                        <button onClick={downloadImage} className="btn download-btn">
-                                            <span className="icon">
-                                                <svg className="css-i6dzq1" stroke-linejoin="round" stroke-linecap="round" fill="none" stroke-width="2" stroke="currentColor" height="24" width="24" viewBox="0 0 24 24"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line y2="3" x2="12" y1="15" x1="12"></line></svg>
-                                            </span>
-                                            <span className="text">Download PDF</span>
-                                        </button>
-                                    </div>
-                                </>
-                            )
-                        })
-                    }
-                </Slider>
-                <div className="arrows-container">
-                    <button className="previousButton" onClick={scrollToPrev}>
-                        <img src={rightArrow} alt="" />
-                    </button>
-                    <button className="nextButton" onClick={scrollToNext}>
-                        <img src={rightArrow} alt="" />
-                    </button>
-                </div>
-            </div>
-        </>
-    )
+						return (
+							<>
+								<div key={lab.id} className={`slide-container`}>
+									<div className='output-container'>
+										<h2>{lab.labName}</h2>
+										<p align='center' style={{ color: "#fff" }}>
+											(You can also edit the path name manually)
+										</p>
+										<div className='output-terminal' id={lab.id}>
+											<blockquote
+												className='output-path'
+												spellCheck='false'
+												contentEditable={true}
+												key={lab.labName}
+											>
+												<span className='user-name'>
+													{name.split(" ")[0]}@laptop
+												</span>
+												<span className='mingw'>MINGW64</span>
+												<span className='file-path'>
+													/f/{name}/{symbol}/lab-files-os
+												</span>
+											</blockquote>
+											<div className='output-trigger'>
+												$ gcc lab-{lab.labCount}.c
+											</div>
+											<pre className='output-main'>{lab.labOutput}</pre>
+										</div>
+									</div>
+									<button onClick={downloadImage} className='btn download-btn'>
+										<span className='icon'>
+											<svg
+												className='css-i6dzq1'
+												stroke-linejoin='round'
+												stroke-linecap='round'
+												fill='none'
+												stroke-width='2'
+												stroke='currentColor'
+												height='24'
+												width='24'
+												viewBox='0 0 24 24'
+											>
+												<path d='M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4'></path>
+												<polyline points='7 10 12 15 17 10'></polyline>
+												<line y2='3' x2='12' y1='15' x1='12'></line>
+											</svg>
+										</span>
+										<span className='text'>Download PDF</span>
+									</button>
+								</div>
+							</>
+						);
+					})}
+				</Slider>
+				<div className='arrows-container'>
+					<button className='previousButton' onClick={scrollToPrev}>
+						<img src={rightArrow} alt='' />
+					</button>
+					<button className='nextButton' onClick={scrollToNext}>
+						<img src={rightArrow} alt='' />
+					</button>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default LabOutputs
+export default LabOutputs;
--- src/details.js
+++ src/details.js
@@ -1,78 +1,78 @@
 const generateRandom = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 const labOutputsDetails = [
-    [
+  [
 
-        {
-            id: "lab-1-output-1",
-            labName: "Lab 1 Output 1",
-            labCount: "1",
-            labOutput:
-                `This code is running in the parent process. ID: ${generateRandom(1300, 1700)}
+    {
+      id: "lab-1-output-1",
+      labName: "Lab 1 Output 1",
+      labCount: "1",
+      labOutput:
+        `This code is running in the parent process. ID: ${generateRandom(1300, 1700)}
 Child Process created with process ID: ${generateRandom(13000, 17000)} `
-        },
-        {
-            id: "lab-1-output-2",
-            labName: "Lab 1 Output 2",
-            labCount: "2",
-            labOutput: `Main thread ID: 1
+    },
+    {
+      id: "lab-1-output-2",
+      labName: "Lab 1 Output 2",
+      labCount: "2",
+      labOutput: `Main thread ID: 1
 Process ID: ${generateRandom(100, 1000)}
 This statement is being printed under thread 3 with ID: 2
 This statement is being printed under thread 3 with ID: 3
 This statement is being printed under thread 4 with ID: 4
 This statement is being printed under thread 0 with ID: 5
 This statement is being printed under thread 1 with ID: 6 `
-        },
-        {
-            id: "lab-1-output-3",
-            labName: "Lab 1 Output 3",
-            labCount: "3",
-            labOutput: `Parent thread started. ID: ${generateRandom(1100, 4000)}
+    },
+    {
+      id: "lab-1-output-3",
+      labName: "Lab 1 Output 3",
+      labCount: "3",
+      labOutput: `Parent thread started. ID: ${generateRandom(1100, 4000)}
 Child thread started. ID: ${generateRandom(4000, 12000)}
 Waiting for child thread to finish...
 Child thread finished.
 Parent thread finished.`
-        },
-    ],
-    [
-        {
-            id: "lab-2-output-1",
-            labName: "Lab 2 Output 1",
-            labCount: "1",
-            labOutput:
-                `This code is running in the parent process. ID: ${generateRandom(1300, 1700)}
+    },
+  ],
+  [
+    {
+      id: "lab-2-output-1",
+      labName: "Lab 2 Output 1",
+      labCount: "1",
+      labOutput:
+        `This code is running in the parent process. ID: ${generateRandom(1300, 1700)}
 Child Process created with process ID: ${generateRandom(13000, 17000)} `
-        },
-        {
-            id: "lab-2-output-2",
-            labName: "Lab 2 Output 2",
-            labCount: "2",
-            labOutput: `Main thread ID: 1
+    },
+    {
+      id: "lab-2-output-2",
+      labName: "Lab 2 Output 2",
+      labCount: "2",
+      labOutput: `Main thread ID: 1
 Process ID: ${generateRandom(100, 1000)}
 This statement is being printed under thread 3 with ID: 2
 This statement is being printed under thread 3 with ID: 3
 This statement is being printed under thread 4 with ID: 4
 This statement is being printed under thread 0 with ID: 5
 This statement is being printed under thread 1 with ID: 6 `
-        },
-        {
-            id: "lab-2-output-3",
-            labName: "Lab 2 Output 3",
-            labCount: "3",
-            labOutput: `Parent thread started. ID: ${generateRandom(1100, 4000)}
+    },
+    {
+      id: "lab-2-output-3",
+      labName: "Lab 2 Output 3",
+      labCount: "3",
+      labOutput: `Parent thread started. ID: ${generateRandom(1100, 4000)}
 Child thread started. ID: ${generateRandom(4000, 12000)}
 Waiting for child thread to finish...
 Child thread finished.
 Parent thread finished.`
-        },
-    ],
-    [
-      {
-        id: "lab-3-output-1",
-        labName: "Lab 3 Output 1",
-        labCount: "3",
-        labOutput: `Enter the no of processes : 2
+    },
+  ],
+  [
+    {
+      id: "lab-3-output-1",
+      labName: "Lab 3 Output 1",
+      labCount: "3",
+      labOutput: `Enter the no of processes : 2
 
 Enter the no of resources : ${generateRandom(1, 10)}
 
@@ -102,8 +102,43 @@ Process 0 runs to completion!
 Process 1 runs to completion!
 The system is in a safe state!!
 Safe Sequence : < 0 1 > `
-      }
-    ]
+    }
+  ],
+  [
+    {
+      id: "lab-5-output-1",
+      labName: "Lab 5 Output 1",
+      labCount: "5",
+      labOutput: `
+                        Memory Management Scheme - First Fit
+Enter the number of blocks:6
+Enter the number of processes:5
+
+Enter the size of the blocks:-
+Block no.1:24
+Block no.2:28
+Block no.3:56
+Block no.4:66
+Block no.5:85
+Block no.6:96
+
+Enter the size of the processes :-
+Process no.1:2
+Process no.2:3
+Process no.3:5
+Process no.4:8
+Process no.5:6
+
+Block no.  size          process no.        size
+1          1997332848    1                  1997308285
+2          24            2                  2
+3          28            3                  3
+4          56            4                  5
+5          66            5                  8
+6          85            Not allocated
+`
+    }
+  ]
 ]
 
 export default labOutputsDetails
